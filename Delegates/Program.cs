bb53942738ba93e423abd04b89820baef1ce6c57
SagarPandita0 <panditasagar99@gmail.com>
2020-08-14 02:05:54 +0530

    Delegates-Basic Example

---
+++ Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Delegates
+{
+    delegate int NumberChanger(int n);
+    class Program
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+       
+       
+        static void Main(string[] args)
+        {
+            //create delegate instances
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            
+
+            //calling the methods using the delegate objects
+           int x =  nc1(25);
+            Console.WriteLine("Value of Num: "+ x);
+
+        }
+    }
+}
