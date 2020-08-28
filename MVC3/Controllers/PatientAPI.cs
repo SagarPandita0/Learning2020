497b446c9ec186fcfcb9595b81e0c8be7c88ab8b
SagarPandita0 <panditasagar99@gmail.com>
2020-08-28 03:07:21 +0000

    table displaying successfully!

--- MVC3/Controllers/PatientAPI.cs
+++ MVC3/Controllers/PatientAPI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using HospitalRepository;
 using Microsoft.AspNetCore.Http;
