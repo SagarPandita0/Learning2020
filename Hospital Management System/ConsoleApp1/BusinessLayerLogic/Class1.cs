2d21ffc42c136a933e196977a90965d6e92b9373
SagarPandita0 <panditasagar99@gmail.com>
2020-09-03 03:45:11 +0000

    hms

--- Hospital Management System/ConsoleApp1/BusinessLayerLogic/Class1.cs
+++ Hospital Management System/ConsoleApp1/BusinessLayerLogic/Class1.cs
@@ -42,7 +42,7 @@ namespace BusinessLayerLogic
         public void MapPatientToNurses(Patient p)
         { // Mapping Problem to medication.
             Patient pt = new Patient();
-            pt.Name = p.Name;           
+            pt._Name = p._Name;           
             Nurse nurse = new Nurse();
             List<string> nurseList = new List<string>();
             //Enter number of nurses to map to Patient
@@ -53,12 +53,12 @@ namespace BusinessLayerLogic
                 nurseList.Add(nurse.Name);
                 x--;
             }
-            pn.Add(pt.Name, nurseList);
+            pn.Add(pt._Name, nurseList);
         }
         public void MapPatientToWardBoys(Patient p)
         { // Mapping Patient to WardBoys
             Patient pt = new Patient();
-            pt.Name = p.Name;
+            pt._Name = p._Name;
             WardBoy wardBoy = new WardBoy();
             List<string> wardBoyList = new List<string>();
             //Enter number of WardBoys to map to Patient
@@ -69,7 +69,7 @@ namespace BusinessLayerLogic
                 wardBoyList.Add(wardBoy.Name);
                 x--;
             }
-            pn.Add(pt.Name, wardBoyList);
+            pn.Add(pt._Name, wardBoyList);
         }
 
         public Doctor doctor;
@@ -78,14 +78,14 @@ namespace BusinessLayerLogic
       // static ListWithDuplicates pts = new ListWithDuplicates();
         static NameValueCollection myCol = new NameValueCollection();
         public int id;      
-        private string _name;       
+            
         public string PrintTime()
         {   
             return(DateTime.Now.ToString("MM/dd/yyyy hh:mm tt"));
         }
         public Patient(string pName)
         { // call this constructor when you wish have timestamp attached.
-            this.Name = pName;     
+            this._Name = pName;     
           //  pts.Add(pName,PrintTime());
             myCol.Add(pName,PrintTime());
             //Console.WriteLine(PrintTime()); 
@@ -106,10 +106,28 @@ namespace BusinessLayerLogic
             //}
             Console.WriteLine("Patient History: {0}",myCol[name]);
         }
-        public void PatientAllergies(Allergies pata) 
+        public void IntakePatientAllergies(Allergies pata) 
         {         
             List<Allergies> allergies = new List<Allergies>();
-            allergies.Add(pata);
+            Dictionary<string, List<Allergies>> ts = new Dictionary<string, List<Allergies>>();
+            //Enter Patient Allergy count
+            int x = Int32.Parse(Console.ReadLine());
+            Allergies allergies1 = new Allergies();
+            //start entering details one by one
+
+            
+            while (x != 0)
+            {
+                
+                allergies1.Name = pata.Name;
+                allergies1.Description = pata.Description;
+                allergies1.NumOfYears = pata.NumOfYears;
+                allergies1.PatientName = pata.PatientName;
+                allergies.Add(allergies1); // this way we added allergies with all required details.
+                allergies1 = null;
+                x--;
+            }
+            ts.Add(allergies1.PatientName, allergies);
         }
         public void PrintPatientAllergies(string pname) 
         {
@@ -117,30 +135,49 @@ namespace BusinessLayerLogic
                                                    
             Console.WriteLine(pPA);
         }
-        public void PatientProblem(Problem patp)
+        public void MapPatientToProblems(Problem patp)
         {
             List<Problem> problems = new List<Problem>();
+            Dictionary<string, List<Problem>> ptop= new Dictionary<string, List<Problem>>();
+            int x = Int32.Parse(Console.ReadLine());
+            Problem problem = new Problem();
+            //start entering details one by one
 
-            problems.Add(patp);
-        }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        private string _PhoneNumber;
-        public string PhoneNumber
-        {
-            get { return _PhoneNumber; }
-            set
+            
+            while (x != 0)
             {
-                Regex expr = new Regex(@"^[6-9]\d{9}$");
+                problem.Name =patp.Name;
+                problem.Description = patp.Description;
+                problem.NumOfDays = patp.NumOfDays;
+                problems.Add(problem);  // this way we added problems with all required details.             
+                problem = null;
+                x--;
+            }
+            ptop.Add(_Name, problems);
+        }
+        private string _Name;
+        private string _Address;
+        public void GetPatientNam(string name)
+        {
+           _Name = name;
+        }
+        public void GetPatientAddress(string address)
+        {
+            _Address = address;
+        }
+        
+        private string _PhoneNumber;
+        public void GetPatientPhoneNumber(string num)
+        {
+            Regex expr = new Regex(@"^[6-9]\d{9}$");
 
-                if (expr.IsMatch(value))
-                {
-                    _PhoneNumber = value;
-                }
-                else
-                {
-                    throw new Exception("Incorrect Number entered");
-                } 
+            if (expr.IsMatch(num))
+            {
+                _PhoneNumber = num;
+            }
+            else
+            {
+                throw new Exception("Incorrect Number entered");
             }
         }
         public void PatientGenerateReport() { 
@@ -149,7 +186,7 @@ namespace BusinessLayerLogic
         }
        
         // this way we will be able to map multiple medications to a single problem.
-        Dictionary<string, List<Medication>> promed = new Dictionary<string, List<Medication>>();
+        static Dictionary<string, List<Medication>> promed = new Dictionary<string, List<Medication>>();
         public void MapProblemToMedication(Problem pro)
         { // Mapping Problem to medication.
             _problem1.Name = pro.Name;
@@ -164,6 +201,21 @@ namespace BusinessLayerLogic
                 x--;
             }
             promed.Add(_problem1.Name, pro.medications);
+        }
+        public List<Medication> DoctorRefersMedicationToPatient(string problem)
+        {
+            //doctor will access the mapped Medication-to-Problem.
+            //Doctor will take in Patient Problem and give medication.
+            string problemWithPatient=problem;
+            foreach( var item in promed)
+            {
+                if (problemWithPatient.Equals(item.Key))
+                {
+                    return item.Value; // this will give the medications that exist for the particular problem.
+                }
+            }
+
+            return null;
         }
     }
     public class Allergies 
@@ -183,38 +235,65 @@ namespace BusinessLayerLogic
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public int NumOfDays { get; set; }
 
-       public List<Medication> medications;//1 problem - many medications.
+        public List<Medication> medications;//1 problem - many medications.
     }
     public class Doctor : Staff
     {
         public Patient patient;
 
-        private string _education;
-        
-        public string Education
-        {
-            get { return _education; }
-            set {if(value != null) _education = value; }
-        }
+        public string Education { get; set; }
 
-        
-       // public override int EncashSalary() => 100000;
+        Doctor doctor = new Doctor();
+        public void AssignEducationToDoctor(string education)
+        {
+            doctor.Education = education;
+        }
        
+       
+        public void AssignNameToDoctor(string dname)
+        {
+            
+            doctor.Name = dname; // this Name comes from Staff.
+        }
+        public void AssignJoiningdate(string ndate)
+        {
+            doctor.JoiningDate = ndate;
+        }
+        // public override int EncashSalary() => 100000;
+
     }
 
     public class Nurse : Staff
     {
         // public override int EncashSalary() => 10000;
-       
-        
+        Nurse nurse = new Nurse();
+        public void AssignNameToNurse(string nname)
+        {
+           
+            nurse.Name = nname; // this Name comes from Staff.
+        }
+        public void AssignJoiningdate(string ndate)
+        {
+            nurse.JoiningDate = ndate;
+        }
     }
 
     public class WardBoy : Staff
     {
-        
-       // public override int EncashSalary() => 5000;
-      
+
+        // public override int EncashSalary() => 5000;
+        WardBoy wardBoy = new WardBoy();
+        public void AssignNameToWardBoy(string wbname)
+        {
+            
+            wardBoy.Name = wbname;// this Name comes from Staff.
+        }
+        public void AssignJoiningdate(string wbdate)
+        {
+            wardBoy.JoiningDate = wbdate;
+        }
     }
 
     public static class StaffFactory
